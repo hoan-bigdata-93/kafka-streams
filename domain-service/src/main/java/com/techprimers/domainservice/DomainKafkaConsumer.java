c58b4594a3166166e668f6002d55f33aa8b6ed0c
HoanNguyen1993 <hoan.nguyen@tca.com.vn>
2021-10-17 19:03:25 +0700

    The first commit

---
+++ domain-service/src/main/java/com/techprimers/domainservice/DomainKafkaConsumer.java
@@ -0,0 +1,20 @@
+package com.techprimers.domainservice;
+
+import java.util.function.Consumer;
+
+import org.apache.kafka.streams.kstream.KStream;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.techprimers.domaincrawler.Domain;
+
+@Configuration
+public class DomainKafkaConsumer {
+
+  @Bean
+  public Consumer<KStream<String, Domain>> domainService() {
+    return kstream -> kstream.foreach((key, domain) -> {
+      System.out.println(String.format("Domain consumed[%s] Status[%s]", domain.getDomain(), domain.isDead()));
+    });
+  }
+}
